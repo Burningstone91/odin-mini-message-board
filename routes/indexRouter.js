2cb639516be157b2f99ad6fa04988360b9847375
Burningstone91 <dimitri.steiner.gl@gmail.com>
2024-10-22 22:55:39 +0200

    Add sample messages and feature to display them

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,22 @@
 import { Router } from "express";
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.render("index"));
+indexRouter.get("/", (req, res) =>
+  res.render("index", { title: "Mini Message Board", messages: messages })
+);
 
 export default indexRouter;
